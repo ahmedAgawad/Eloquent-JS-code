388d53eb7fbc9aacae99ddc185ebd3269af79371
Ahmed Abdelgawad <ahmed.m.elshennawi@gmail.com>
2026-09-24 21:53:55 +0300

    Chapter 4 : reversing array exercise

---
+++ chapter4-data-structures/book-exercises/reversing_array.js
@@ -0,0 +1,26 @@
+function reverseArray(arr) {
+	let reversedCopy = [];
+	for (let elem of arr) {
+		reversedCopy.unshift(elem);
+	}
+
+	return reversedCopy;
+}
+
+function reverseArrayInPlace(arr) {
+	let start = 0;
+	let end = arr.length - 1;
+	while (start < end) {
+		let temp = arr[start];
+		arr[start] = arr[end];
+		arr[end] = temp;
+		start++;
+		end--;
+	}
+
+	return arr;
+}
+
+console.log(reverseArray([1, 2, 3, 4, 5]));
+console.log(reverseArrayInPlace([1, 2, 3, 4, 5]));
+console.log(reverseArrayInPlace([1, 2, 3, 4]));
